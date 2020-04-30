5a2bbf5be228af636aebe6ed9c4078a081e86dac
brandon <bbarks15@gmail.com>
2020-04-30 17:49:56 +0800

    merged brandons flask code

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,14 @@
-#### TO ADD: Referencing
-
-import os
-
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    from . import db, auth
-    db.init_app(app)
-    app.register_blueprint(auth.bp)
-
-    return app
+from app import routes, models
