e5143d93c5952ec03e4a159ec208b05772069b04
chenga3 <a.cheng13@student.unimelb.edu.au>
2020-05-23 22:02:26 +0800

    token authentication

--- app/models.py
+++ app/models.py
@@ -3,7 +3,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from flask import current_app
 from hashlib import md5
-import datetime
+from datetime import datetime, timedelta, date
+import base64, os
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +13,8 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     admin = db.Column(db.Boolean, index=True, nullable=False)
     points = db.Column(db.Integer, default=0)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
     
     def __repr__(self):
         return '<User {}>'.format(self.username)    
@@ -28,6 +31,25 @@ class User(UserMixin, db.Model):
             digest, size)
 
 
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+    
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
@@ -39,7 +61,7 @@ class Problem(db.Model):
     difficulty = db.Column(db.Integer)
     numAttempts = db.Column(db.Integer, default=0)
     numSuccesses = db.Column(db.Integer, default=0)
-    dateAdded = db.Column(db.String(10), default = datetime.date.today().strftime("%d/%m/%Y"))
+    dateAdded = db.Column(db.String(10), default = date.today().strftime("%d/%m/%Y"))
     body= db.Column(db.Text())
     timeLimit = db.Column(db.Float)
     testCases = db.relationship('ProblemTestCases', backref="problem")
--- app/static/admin/admin.js
+++ app/static/admin/admin.js
@@ -1,5 +1,137 @@
 // This js file is for admin functions in all admin pages
+<<<<<<< HEAD
 
+=======
+/*** AJAX for REST API ***/
+var usertable, user, base_url;
+var authToken = null;
+
+window.onload = function() {
+    this.setUp();
+    base_url = window.location.origin;
+}
+
+function setUp() {
+    usertable = document.getElementById('usertable-api');
+    console.log('setup...');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            responseData = JSON.parse(this.responseText);
+            authToken = responseData['token'];
+            if (usertable != null) {
+                getUsers();
+            }
+        }
+    }
+    xhttp.open('POST', '/api/tokens', true);
+    xhttp.send();
+}
+
+function getUsers() {
+    console.log('getting users...');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        //console.log(this.readyState);
+        //console.log(this.status);
+        if (this.readyState == 4 && this.status == 200) {
+            responseData = JSON.parse(this.responseText);
+            userList = responseData['userList'];
+            renderTable(userList);
+        }
+    }
+    xhttp.open('GET', '/api/users', true);
+    xhttp.setRequestHeader("Authorization", "Bearer " + authToken);
+    xhttp.send();
+}
+
+/* function editUser(id) {
+    console.log('Editing user...');
+    console.log(id);
+    usertable.hidden = true;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            responseData = JSON.parse(this.responseText);
+            user = responseData;
+            console.log(user);
+        }
+    }
+    xhttp.open('GET', '/api/users/' + id, true);
+    xhttp.send();
+} */
+
+function deleteUser(id) {
+    console.log(id);
+    console.log('Deleting user ' + id + '...');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // responseData = JSON.parse(this.responseText);
+            // userList = responseData['userList'];
+            tr = document.getElementById('user' + id);
+            tr.remove();
+        }
+    }
+    xhttp.open('DELETE', '/api/users/' + id, true);
+    xhttp.setRequestHeader("Authorization", "Bearer " + authToken);
+    xhttp.send();
+}
+
+function renderTable(userList) {
+    for (var i=0; i<userList.length; i++) {
+        console.log(userList[i]);
+        tr = document.createElement('tr');
+        tr.id = 'user' + userList[i].id;
+
+        td = document.createElement('td');
+        td.innerHTML = userList[i].username;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = userList[i].email;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = (userList[i].admin) ? "Admin" : "User";
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        button = document.createElement('button');
+        button.innerHTML = 'edit';
+        button.classList.add('edit');
+        button.value = userList[i].id;
+        button.onclick = function(ev) { location.href = base_url + '/admin/' + ev.target.value + '/edituser'; };
+        td.appendChild(button);
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        button = document.createElement('button');
+        button.innerHTML = 'delete';
+        button.classList.add('delete');
+        button.value = userList[i].id;
+        button.onclick = function(ev) { 
+                var result = confirm("Are you sure you want to delete");
+                if (result) {
+                    deleteUser(ev.target.value);
+                }
+            }
+        td.appendChild(button);
+        tr.appendChild(td);
+
+        usertable.appendChild(tr);
+    }
+}
+
+function renderUserForm(user) {
+    
+}
+
+
+
+/*** OTHER FUNCTIONS ***/
+>>>>>>> c1d62354... token authentication
 //Adding a new user
 function addUser() {
     var username = document.getElementById("username").value.trim();
