a094872c9a2dbfceca847cc9a23034d4f31aaf44
brandon <bbarks15@gmail.com>
2020-05-25 16:11:45 +0800

    shell script added

---
+++ worker.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['yeetcode-judge']
+
+conn = redis.from_url('redis://')
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work(logging_level='WARN')
+
+
+
